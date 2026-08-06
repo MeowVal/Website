32b844ce23ba6f1eb869945cbf1c3118f551ddcb
MeowVal <meowval@catkatnya.com>
2026-08-06 02:47:03 +0200

    simple nav bar

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,31 @@
 
 <template>
   <!--<HelloWorld />-->
+  <nav class="topbar">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/projects">Projects</router-link>
+    <router-link to="/contact">Contact</router-link>
+    <router-link to="/PrivacyPolicy">Privacy</router-link>
+  </nav>
   <router-view />
 </template>
+
+<style scoped>
+.topbar {
+  display: flex;
+  gap: 20px;
+  padding: 10px;
+  background: #222;
+}
+
+.topbar a {
+  color: #ccc;
+  text-decoration: none;
+  font-weight: 500;
+}
+.router-link-active {
+  color: #fff;
+  border-bottom: 2px solid #fff;
+}
+</style>
